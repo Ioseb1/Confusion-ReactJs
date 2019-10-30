77b5f58b915410874fa66c5ebbda68f40f021436
Ioseb Matiashvili <imatiashvili3@gmail.com>
2019-10-31 00:37:40 +0400

    Components Part1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 function App() {
   return (
     // My first coding in ReactJS starts here. Wish me luck!!!. Imedia gamomiva
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
